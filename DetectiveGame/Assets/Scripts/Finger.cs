3bea5f84b9aded1787f41136b250401f1459f1b6
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-10-12 15:05:41 +0900

    Final_version
    최종 제출용 버전

---
+++ DetectiveGame/Assets/Scripts/Finger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finger : MonoBehaviour
+{
+    public GameObject kakaoWindow;
+    public GameObject basicWindow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(col);
+        if (col.transform.tag == "Kakaotalk")
+        {
+            basicWindow.SetActive(false);
+            kakaoWindow.SetActive(true);
+        }
+    }
+}
---
+++ DetectiveGame/Assets/Scripts/PhoneLock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneLock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "FingerPrint")
+        {
+            Debug.Log("잠금 해제");
+        }
+    }
+}
